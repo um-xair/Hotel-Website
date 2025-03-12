d7ce8972d53d9ca3013c30fe847ed202b3ceac49
Alyssa <natasyahebat@gmail.com>
2025-03-12 16:08:53 +0800

    12-3-2025

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+    ScrollReveal().reveal('#bottom', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 2000 
+    });
+
+    ScrollReveal().reveal('#left', { 
+        origin: 'left', 
+        distance: '80px', 
+        duration: 2000 
+    });
+
+    ScrollReveal().reveal('#right', { 
+        origin: 'right', 
+        distance: '80px', 
+        duration: 2000 
+    });
+
+    ScrollReveal().reveal('#bottom1', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 2000 
+    });
+
+    ScrollReveal().reveal('#bottom2', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 2500 
+    });
+
+    ScrollReveal().reveal('#bottom3', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 3000 
+    });
+
+    ScrollReveal().reveal('#bottom4', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 3500 
+    });
+
+    ScrollReveal().reveal('#bottom5', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 4000 
+    });
+
+    ScrollReveal().reveal('#bottom6', { 
+        origin: 'bottom', 
+        distance: '80px', 
+        duration: 4500 
+    });
+});
